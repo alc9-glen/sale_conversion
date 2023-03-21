8a39ef09c0495dc782b77a9b7ec90c8b2b05cb82
GlebwithB <glencshi8@gmail.com>
2023-03-21 18:47:08 -0400

    open function
    open all files

---
+++ functions.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt, pandas as pd, numpy as np, seaborn as sns
+from sklearn import svm, linear_model
+from sklearn.linear_model import Perceptron
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.linear_model import LogisticRegression
+
